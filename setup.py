3bffff49caad246176f3315a2e8a76acab241975
GavrilovSD <sergeigavrilov123@gmil.com>
2018-02-20 16:10:02 +0300

    add setup for distutil

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='simple-orm-lite',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['sqliteorm.py'],
+    Author = 'Sergey'
+)
